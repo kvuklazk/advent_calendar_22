485cb5a860e247c8950bd166d95bfa4064ff586a
Jakub Svatuška <kuba.svatuska@gmail.com>
2022-12-30 15:57:24 +0100

    update

--- 9/9_1.py
+++ 9/9_1.py
@@ -1,26 +1,55 @@
 inp = [a.strip().split() for a in open("input.txt", "r").readlines()]
 inp = [[a[0], int(a[1])] for a in inp]
 
-s = []
+h = [0, 0]  # (column, row)
+t = [0, 0]
 
-for i in range(10):
-    s.append([])
-    for k in range(10):
-        s[i].append(".")
-s[0][0] = "H"
-print(s)
 
 def move_right():
-    pass
+    if t[0] < h[0]:
+        t[0] += 1
+        if t[1] < h[1]:
+            t[1] += 1
+        elif t[1] > h[1]:
+            t[1] -= 1
+    h[0] += 1
+
+
 def move_left():
-    pass
+    if t[0] > h[0]:
+        t[0] -= 1
+        if t[1] < h[1]:
+            t[1] += 1
+        elif t[1] > h[1]:
+            t[1] -= 1
+    h[0] -= 1
+
+
 def move_up():
-    pass
+    if t[1] < h[1]:
+        t[1] += 1
+        if t[0] < h[0]:
+            t[0] += 1
+        elif t[0] > h[0]:
+            t[0] -= 1
+    h[1] += 1
+
+
 def move_down():
-    pass
+    if t[1] > h[1]:
+        t[1] -= 1
+        if t[0] < h[0]:
+            t[0] += 1
+        elif t[0] > h[0]:
+            t[0] -= 1
+    h[1] -= 1
+
+
+tail_visited = [[0, 0]]
 
 for instruction in inp:
-    print(instruction)
+    print(instruction, h, t)
+    print("begoin", tail_visited)
     for i in range(instruction[1]):
         if instruction[0] == 'R':
             move_right()
@@ -30,3 +59,10 @@ for instruction in inp:
             move_up()
         else:
             move_down()
+    print(t)
+    print("b", tail_visited)
+    tail_visited.append(t)
+    print("a", tail_visited)
+
+print(h, t)
+print(tail_visited)
