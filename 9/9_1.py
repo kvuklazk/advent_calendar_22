638b85141b954759b55bd8027e04d7ae230ec4ba
kvukvak <66948246+kvuklazk@users.noreply.github.com>
2022-12-31 22:06:18 +0100

    Update 9_1.py

--- 9/9_1.py
+++ 9/9_1.py
@@ -1,11 +1,9 @@
 inp = [a.strip().split() for a in open("input.txt", "r").readlines()]
 inp = [[a[0], int(a[1])] for a in inp]
 
-h = [0, 0]  # (column, row)
-t = [0, 0]
-
 
 def move_right():
+
     if t[0] < h[0]:
         t[0] += 1
         if t[1] < h[1]:
@@ -36,20 +34,21 @@ def move_up():
 
 
 def move_down():
+
     if t[1] > h[1]:
         t[1] -= 1
         if t[0] < h[0]:
             t[0] += 1
         elif t[0] > h[0]:
             t[0] -= 1
+
     h[1] -= 1
 
 
-tail_visited = [[0, 0]]
+tail_visited = []
+h, t = [0, 0], [0, 0]
 
 for instruction in inp:
-    print(instruction, h, t)
-    print("begoin", tail_visited)
     for i in range(instruction[1]):
         if instruction[0] == 'R':
             move_right()
@@ -59,10 +58,40 @@ for instruction in inp:
             move_up()
         else:
             move_down()
-    print(t)
-    print("b", tail_visited)
-    tail_visited.append(t)
-    print("a", tail_visited)
 
+        tail_visited.append([a for a in t])
+
+t_v = []
+
+for v in tail_visited:
+    if v not in t_v:
+        t_v.append(v)
 print(h, t)
-print(tail_visited)
+print(t_v)
+#
+# biggest_x = 0
+# biggest_y = 0
+
+# for i in t_v:
+#     if i[0] > biggest_x:
+#         biggest_x = i[0]
+#
+# for i in t_v:
+#     if i[1] > biggest_y:
+#         biggest_y = i[1]
+
+# print_list = []
+#
+# for i in range(biggest_y+1):
+#     print_list.append([])
+#     for k in range(biggest_x+1):
+#         print_list[i].append(".")
+#
+# for x, y in t_v:
+#     print_list[y][x] = "#"
+#
+# print_list.reverse()
+# for i in print_list:
+#     print(i)
+
+print(len(t_v))
